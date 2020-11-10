89961061155f0135e7a07a1936b818f760925e9f
Harsh Vardhan <38508679+vardhah@users.noreply.github.com>
2020-11-10 16:52:55 -0600

    Add files via upload

---
+++ Matpy/testing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 16:24:26 2019
+
+@author: vardhah
+"""
+
+import matplotlib.pyplot as plt
+
+def add_number(u):
+    return u*2; 
+    plt.plot(u);
