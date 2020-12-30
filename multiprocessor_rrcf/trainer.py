0699aeae48c62f0fc1b7bf82a83c2db3d4737110
Harsh Vardhan <38508679+vardhah@users.noreply.github.com>
2020-12-29 18:56:06 -0600

    Add files via upload

--- multiprocessor_rrcf/trainer.py
+++ multiprocessor_rrcf/trainer.py
@@ -9,26 +9,24 @@ import time
 
 
 
-treek = [None] * 100
+tree = [None] * 100
+forest=None 
 
 def f(i,x):
-	global treek 
-	treek[i]=RCTree(x)
 	return RCTree(x) 
 
 def dispf(i):
-	global treek
-	#print(tree)
-	return pd.Series({leaf : treek[i].disp(leaf) for leaf in treek[i].leaves}).sort_index()
+	return pd.Series({leaf : forest[i].disp(leaf) for leaf in forest[i].leaves}).sort_index()
 
 #if __name__ == '__main__':
 def main():
  #initialisation(number of tree)
  num_trees = 100
  global tree
+ global forest
  #Data loading and reshaping
  train_data=pd.read_csv('r_train_small.csv')
- train_np_data=train_data.values
+ train_np_data=train_data.values[0:100]
  X=train_data.values
  print("Shape of X:",X.shape)
  print("Total data Size:",train_np_data.shape[0] ,",",train_np_data.shape[1])
@@ -55,47 +53,50 @@ def main():
  print(len(tree))
  for i in range(num_trees):
     forest.append(tree[i])
- print("length of forest:",len(forest))
+ #print("length of forest:",len(forest),forest)
  end=time.time()
  print("time elapsed for tree creation is:",end-start)
 
 
- start=time.time()
- avg_disp = pd.Series(0.0, index=np.arange(n))
- index = np.zeros(n)
- for t in forest: 
-    disp = pd.Series({leaf : t.disp(leaf)
-                       for leaf in t.leaves})
-    avg_disp[disp.index] += disp
-    np.add.at(index, disp.index.values, 1)
- avg_disp /= index
- end=time.time()
- print("elapsed time:",end-start)
+
  
 
  start=time.time()
- avg_disp1 = pd.Series(0.0, index=np.arange(n))
+ avg_disp = pd.Series(0.0, index=np.arange(n))
  for t in forest: 
     disp1 = pd.Series({leaf : t.disp(leaf)
                        for leaf in t.leaves}).sort_index()
-    avg_disp1 += disp1
+    avg_disp += disp1
  end=time.time()
  print("elapsed time1:",end-start) 
+ peak_val=avg_disp.max()
  
- start=time.time()
- with Pool(processes=num_processor) as pool:
-        displist=pool.map(dispf, data_list)  
- #avg_disp1 /= num_trees
- #print("Length of average disp:",len(avg_disp1))
- #print("avg disp is:",avg_disp1)
- end=time.time()
- print("elapsed time1:",end-start) 
 
- #plt.plot(avg_disp-avg_disp1)
- #plt.hlines(25,1,1100,'r')
- plt.grid()
- plt.show()
- print("Max disp: ",avg_disp.max())
+ for i in range(50):
+  new_testpoint=train_np_data[(100+i)]
+  point_disp=0
+  ttrees=0
+  for tree in forest:
+    tree.insert_point(new_testpoint,index=5986+i)
+    ttrees+=1
+  for tree in forest: 
+    disp =  tree.disp(5986+i)                   
+    point_disp += disp
+  point_disp=point_disp/len(forest)
+  disp_val.append(point_disp)
+  print('-->New point disp:',point_disp)
+  if point_disp<peak_val:
+    for tree in forest: 
+     tree.forget_point(index=5986+i)
+    print("removed point")
+    cnt+=1 
+ else:
+    print("point accepted in tree") 
+    peak_val=point_disp
+    cntt+=1   
+
+
 
 if __name__ == '__main__':
+	
 	main()
